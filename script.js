dfe5dca58ffabb459c658620a63c382bd10d404b
victor13135 <stefan.vasilov@gmail.com>
2020-10-28 11:34:02 +0100

    initialRelease

---
+++ script.js
@@ -0,0 +1,322 @@
+let film1 = {
+    name: "I Origins",
+    director: "Mike Cahill",
+    year: 2014,
+    duration: 106,
+    info: "A molecular biologist and his laboratory partner uncover evidence that may fundamentally change society as we know it.",
+    thumbnail: "img/iOrigins.jpg",
+    imdbLink: "https://www.imdb.com/title/tt2884206"
+};
+let film2 = {
+    name: "Fight Club",
+    director: "David Fincher",
+    year: 1999,
+    duration: 139,
+    info: "An insomniac office worker and a devil-may-care soapmaker form an underground fight club that evolves into something much, much more.",
+    thumbnail: "img/fightClub.jpg",
+    imdbLink: "https://www.imdb.com/title/tt0137523"
+};
+let film3 = {
+    name: "Dunkirk",
+    director: "Christopher Nolan",
+    year: 2017,
+    duration: 106,
+    info: "Allied soldiers from Belgium, the British Empire, and France are surrounded by the German Army and evacuated during a fierce battle in World War II.",
+    thumbnail: "img/dunkirk.jpg",
+    imdbLink: "https://www.imdb.com/title/tt5013056"
+};
+let film4 = {
+    name: "The Usual Suspects",
+    director: "Bryan Singer",
+    year: 1995,
+    duration: 106,
+    info: "A sole survivor tells of the twisty events leading up to a horrific gun battle on a boat, which began when five criminals met at a seemingly random police lineup.",
+    thumbnail: "img/theUsualSuspects.jpg",
+    imdbLink: "https://www.imdb.com/title/tt0114814"
+};
+let film5 = {
+    name: "Se7en",
+    director: "David Fincher",
+    year: 1995,
+    duration: 127,
+    info: "Two detectives, a rookie and a veteran, hunt a serial killer who uses the seven deadly sins as his motives.",
+    thumbnail: "img/se7en.jpg",
+    imdbLink: "https://www.imdb.com/title/tt0114369"
+};
+let film6 = {
+    name: "Memento",
+    director: "Christopher Nolan",
+    year: 2000,
+    duration: 113,
+    info: "A man with short-term memory loss attempts to track down his wife's murderer.",
+    thumbnail: "img/memento.jpg",
+    imdbLink: "https://www.imdb.com/title/tt0209144"
+};
+let film7 = {
+    name: "Goodfellas",
+    director: "Martin Scorsese",
+    year: 1990,
+    duration: 146,
+    info: "The story of Henry Hill and his life in the mob, covering his relationship with his wife Karen Hill and his mob partners Jimmy Conway and Tommy DeVito in the Italian-American crime syndicate.",
+    thumbnail: "img/goodfellas.jpg",
+    imdbLink: "https://www.imdb.com/title/tt0099685"
+};
+let film8 = {
+    name: "The Curious Case of Benjamin Button",
+    director: "David Fincher",
+    year: 2008,
+    duration: 166,
+    info: "Tells the story of Benjamin Button, a man who starts aging backwards with consequences.",
+    thumbnail: "img/theCuriousCaseOfBenjaminButton.jpg",
+    imdbLink: "https://www.imdb.com/title/tt0421715"
+};
+let film9 = {
+    name: "The Dark Knight",
+    director: "Christopher Nolan",
+    year: 2008,
+    duration: 153,
+    info: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+    thumbnail: "img/theDarkKnight.jpg",
+    imdbLink: "https://www.imdb.com/title/tt0468569"
+};
+let film10 = {
+    name: "Shutter Island",
+    director: "Martin Scorsese",
+    year: 2010,
+    duration: 138,
+    info: "In 1954, a U.S. Marshal investigates the disappearance of a murderer who escaped from a hospital for the criminally insane.",
+    thumbnail: "img/shutterIsland.jpg",
+    imdbLink: "https://www.imdb.com/title/tt1130884"
+};
+let film11 = {
+    name: "There Will Be Blood",
+    director: "Paul Thomas Anderson",
+    year: 2007,
+    duration: 158,
+    info: "A story of family, religion, hatred, oil and madness, focusing on a turn-of-the-century prospector in the early days of the business.",
+    thumbnail: "img/thereWillBeBlood.jpg",
+    imdbLink: "https://www.imdb.com/title/tt0469494"
+};
+let film12 = {
+    name: "Apt Pupil",
+    director: "Bryan Singer",
+    year: 1998,
+    duration: 111,
+    info: "A boy blackmails his neighbor after suspecting him to be a Nazi war criminal.",
+    thumbnail: "img/aptPupil.jpg",
+    imdbLink: "https://www.imdb.com/title/tt0118636"
+};
+let film13 = {
+    name: "The Departed",
+    director: "Martin Scorsese",
+    year: 2006,
+    duration: 151,
+    info: "An undercover cop and a mole in the police attempt to identify each other while infiltrating an Irish gang in South Boston.",
+    thumbnail: "img/theDeparted.jpg",
+    imdbLink: "https://www.imdb.com/title/tt0407887"
+};
+let film14 = {
+    name: "No Country for Old Men",
+    director: "Coen brothers",
+    year: 2007,
+    duration: 122,
+    info: "Violence and mayhem ensue after a hunter stumbles upon a drug deal gone wrong and more than two million dollars in cash near the Rio Grande.",
+    thumbnail: "img/noCountryForOldMen.jpg",
+    imdbLink: "https://www.imdb.com/title/tt0477348"
+};
+let film15 = {
+    name: "Gone Girl",
+    director: "David Fincher",
+    year: 2014,
+    duration: 149,
+    info: "With his wife's disappearance having become the focus of an intense media circus, a man sees the spotlight turned on him when it's suspected that he may not be innocent.",
+    thumbnail: "img/goneGirl.jpg",
+    imdbLink: "https://www.imdb.com/title/tt2267998"
+};
+let film16 = {
+    name: "The Thin Red Line",
+    director: "Terrence Malick",
+    year: 1998,
+    duration: 170,
+    info: "Adaptation of James Jones' autobiographical 1962 novel, focusing on the conflict at Guadalcanal during the second World War.",
+    thumbnail: "img/theThinRedLine.jpg",
+    imdbLink: "https://www.imdb.com/title/tt0120863"
+}
+let film17 = {
+    name: "Eyes Wide Shut",
+    director: "Stanley Kubrick",
+    year: 1999,
+    duration: 159,
+    info: "A New York City doctor embarks on a harrowing, night-long odyssey of sexual and moral discovery after his wife reveals a painful secret to him.",
+    thumbnail: "img/eyesWideShut.jpg",
+    imdbLink: "https://www.imdb.com/title/tt0120663"
+}
+let film18 = {
+    name: "Melancholia",
+    director: "Lars von Trier",
+    year: 2011,
+    duration: 135,
+    info: "Two sisters find their already strained relationship challenged as a mysterious new planet threatens to collide with Earth.",
+    thumbnail: "img/melancholia.jpg",
+    imdbLink: "https://www.imdb.com/title/tt1527186"
+}
+let film19 = {
+    name: "Back to the Future",
+    director: "Robert Zemeckis",
+    year: 1985,
+    duration: 116,
+    info: "Marty McFly, a 17-year-old high school student, is accidentally sent thirty years into the past in a time-traveling DeLorean invented by his close friend, the eccentric scientist Doc Brown.",
+    thumbnail: "img/backToTheFuture.jpg",
+    imdbLink: "https://www.imdb.com/title/tt0088763"
+}
+let film20 = {
+    name: "Contact",
+    director: "Robert Zemeckis",
+    year: 1997,
+    duration: 150,
+    info: "Dr. Ellie Arroway, after years of searching, finds conclusive radio proof of extraterrestrial intelligence, sending plans for a mysterious machine.",
+    thumbnail: "img/contact.jpg",
+    imdbLink: "https://www.imdb.com/title/tt0118884"
+}
+
+let films = [];
+for (i=1; i<=20; i++) {
+    let temp = "film" + i;
+    films.push(eval(temp));
+}
+console.log(films[0].director);
+function printDuration( minutes ) {
+    let temp = "";
+    let mins = minutes % 60;
+    let hrs = Math.trunc(minutes/60);
+    temp = `${hrs}h ${mins}m`;
+    return temp;
+}
+
+let newTable = document.createElement("table");
+document.querySelector("body").appendChild(newTable);
+let newHeader = document.createElement("tr");
+let newHeading = document.createElement("th");
+newHeading.colSpan = 2;
+newHeading.padding = "20px";
+newHeading.style.backgroundColor = "grey";
+newHeading.style.color = "white";
+newHeading.textContent = "My IMDB Top 20";
+newHeading.style.textAlign="center";
+newHeader.appendChild(newHeading);
+newTable.appendChild(newHeader);
+
+films.forEach ( (film,i) => {
+    let newRow = document.createElement("tr");
+    let leftData = document.createElement("td");
+    leftData.classList.add("leftData");
+    leftData.style.padding="5px";
+    let imgLink = document.createElement("a");
+    let image = document.createElement("img");
+    image.src = film.thumbnail;
+    imgLink.appendChild(image);
+    imgLink.href=film.imdbLink;
+    leftData.appendChild(imgLink);
+    newRow.appendChild(leftData);
+
+    let rightData = document.createElement("td");
+    rightData.style.padding="25px";
+    let titleLink = document.createElement("a");
+    titleLink.href = film.imdbLink;
+    titleLink.target = "_blank";
+    titleLink.textContent = film.name + " (" + film.year + ")";
+    titleLink.style.textDecoration="none";
+    titleLink.style.fontSize="125%";
+    if (film.duration > 120) {
+        titleLink.style.fontWeight="bold";
+    }
+    rightData.appendChild(titleLink);
+    let directorPara = document.createElement("p");
+    directorPara.textContent = film.director;
+    rightData.appendChild(directorPara);
+    let titlePara = document.createElement("p");
+    titlePara.textContent = film.info;
+    rightData.appendChild(titlePara);
+    let durationPara = document.createElement("p");
+    durationPara.textContent = printDuration(film.duration);
+    rightData.appendChild(durationPara);
+    newRow.appendChild(rightData);
+    newTable.appendChild(newRow);
+    if (i%2 == 0) {
+        leftData.style.backgroundColor="grey";
+        rightData.style.backgroundColor="yellow";
+        image.alt=film.thumbnail;
+    } else {
+        leftData.style.backgroundColor="yellow";
+        rightData.style.backgroundColor="grey";
+    }
+});
+
+// 1. Najduži naziv filma
+let div1 = document.createElement("div");
+let maxTitleLength = 0;
+let indexMax = 0;
+console.log(maxTitleLength);
+films.forEach ( (film,i) => {
+    if (film.name.length > maxTitleLength) {
+        maxTitleLength = film.name.length;
+        indexMax = i;
+    }    
+});
+console.log(indexMax);
+div1.textContent = "Najduži naziv filma je: ";
+div1.textContent+= films[indexMax].name;
+document.querySelector("body").appendChild(div1);
+
+// 2. Najučestalije ime režisera
+let div2 = document.createElement("div");
+let counterArray = [];
+let flag;
+for (let i = 0; i < 20; i++) {
+    let counter = 0;
+    for (let j = 0; j < 20; j++) {
+        if (films[i].director == films[j].director) {
+            counter++;
+        }
+    }
+    counterArray.push(counter);
+}
+// console.log(counterArray);
+let max = counterArray[0];
+let maxIndex = 0;
+for (i=1; i<20; i++) {
+    if (counterArray[i] > max) {
+        max = counterArray[i];
+        maxIndex = i;
+    }
+}
+div2.textContent = "Najučestaliji režiser je: ";
+div2.textContent+= films[maxIndex].director;
+document.querySelector("body").appendChild(div2);
+
+// 3. Broj filmova koji traju duže od dva sata
+let div3 = document.createElement("div");
+let twoHoursPlusCounter = 0;
+films.forEach ( (film) => {
+    if (film.duration > 120) {
+        twoHoursPlusCounter++;
+    }
+});
+div3.textContent = "Broj filmova koji traju duže od dva sata: ";
+div3.textContent+= twoHoursPlusCounter;
+document.querySelector("body").appendChild(div3);
+
+// 4. Broj sličica koje imaju zadat alt tekst
+let div4 = document.createElement("div");
+let imgArray = document.querySelectorAll("img");
+let counter = 0;
+imgArray.forEach( (im) => {
+    if (im.alt != "") {
+        counter++;
+    }
+});
+console.log(counter);
+div4.textContent = "Broj sličica koje imaju zadat alt tekst: ";
+div4.textContent+= counter;
+document.querySelector("body").appendChild(div4);
+console.log(div4);
